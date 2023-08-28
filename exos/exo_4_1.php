fcce093e34afb22ab2f8c0e58d74575c0769cc79
59011-82-08 <59011-82-08@exchange.ad.afpanet>
2023-08-28 16:01:19 +0200

    update 280820231600

--- exos/exo_4_1.php
+++ exos/exo_4_1.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Undocumented class
@@ -23,49 +23,39 @@ class build
      *
      * @return void
      */
-    private function init() : void
+    private function init(): void
     {
         $lineNumbers = 1;
         $letter = 0;
-        for($x = 0; $x <= $this->width * 2; $x++){
-        $line = [];
-        for($j = 0; $j <= $this->height * 2; $j++){
-            if($x % 2 == 0 && $x != 0)
-            {
-                if($j % 2 == 0)
-                {
-                    $line[$j] = '|';
-                } else  {
-                    $line[$j] =  '-';
-                }
-                
-            } 
-            else 
-            {
-                if($x == 0){
-                    if($j % 2 != 0){
-                        $line[$j] = chr(65 + $letter) ;
-                        $letter ++;
+        for ($x = 0; $x <= $this->width * 2; $x++) {
+            $line = [];
+            for ($j = 0; $j <= $this->height * 2; $j++) {
+                if ($x % 2 == 0 && $x != 0) {
+                    if ($j % 2 == 0) {
+                        $line[$j] = '|';
                     } else {
-                        $line[$j] = '|';
+                        $line[$j] =  '-';
                     }
-                    
                 } else {
-                    if($j % 2 == 0)
-                    {
-                        $line[$j] = '|';
-                    } 
-                    else  {
-                        $line[$j] =  $lineNumbers;
-                        $lineNumbers ++;
+                    if ($x == 0) {
+                        if ($j % 2 != 0) {
+                            $line[$j] = chr(65 + $letter);
+                            $letter++;
+                        } else {
+                            $line[$j] = '|';
+                        }
+                    } else {
+                        if ($j % 2 == 0) {
+                            $line[$j] = '|';
+                        } else {
+                            $line[$j] =  $lineNumbers;
+                            $lineNumbers++;
+                        }
                     }
                 }
             }
-            
-          
+            $this->array[$x] = $line;
         }
-        $this->array[$x] = $line;
-    }
     }
 
     /**
@@ -73,7 +63,7 @@ class build
      *
      * @return void
      */
-    public function printArray() : void
+    public function printArray(): void
     {
         print_r($this->array);
     }
@@ -84,24 +74,89 @@ class build
      * @param [type] $value
      * @return array
      */
-    public function searchValue($value) : void
+    public function searchValue($value): void
     {
-        for($x = 0; $x <= $this->width * 2; $x++)
-        {
-            for($j = 0; $j <= $this->height * 2; $j++)
-            {
-               
-                if($this->array[$x][$j] == $value)
-                {
+        for ($x = 0; $x <= $this->width * 2; $x++) {
+            for ($j = 0; $j <= $this->height * 2; $j++) {
+
+                if ($this->array[$x][$j] == $value) {
                     echo "x : " . ceil($j / 2) . PHP_EOL;
                     echo "y : " . ceil($x / 2) . PHP_EOL;
                 }
-                
             }
         }
     }
 }
 
+// Interface utilisateur
+class Menu
+{
+    public static $lines = 0;
+    public static $columns = 0;
 
-$plateau = new build(5, 5);
-print_r($plateau->searchValue(8));
+    public static function buildTab(): void
+    {
+        do {
+            self::$lines = readline("nombre de lignes : ");
+            self::$columns = readline("nombre de colonnes : ");
+        } while (self::$lines <= 0 && self::$columns <= 0 && !preg_match("/^[\d]+$/", self::$columns) && !preg_match("/^[\d]+$/", self::$lines));
+    }
+}
+
+
+// print_r($plateau->searchValue(8));
+
+// Menu::buildTab();
+// echo Menu::$lines . PHP_EOL;
+// echo Menu::$columns . PHP_EOL;
+// $plateau = new build(Menu::$lines, Menu::$columns);
+// $plateau->printArray();
+
+/**
+ * Gestion du tableau à une dimension
+ */
+class SimpleArray
+{
+    private array $array;
+
+    public function __construct(array $array = [])
+    {
+
+    }
+}
+
+
+class SimpleArrayBuilder
+{
+    public static $numbersTab = [];
+    
+
+    public static function buildTab(): void
+    {
+ 
+            do
+            {
+                $number = readline("Entrez un entier : ");
+                if($number != 0 && preg_match("/^[\d]+$/", $number))
+                self::$numbersTab[] = $number;
+            }
+            while($number != 0);
+            
+   
+    }
+
+    public static function printTab(): void
+    {
+        print_r(self::$numbersTab);
+    }
+
+    public static function sortTab(): void
+    {
+        sort(self::$numbersTab);
+        print_r(self::$numbersTab);
+    }
+}
+
+SimpleArrayBuilder::buildTab();
+SimpleArrayBuilder::printTab();
+SimpleArrayBuilder::sortTab();
