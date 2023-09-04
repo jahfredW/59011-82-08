efdb8414a1c85ceae9799318a379fd1f8a4203c9
fred <fred.gruwe@gmail.com>
2023-09-04 15:55:21 +0200

    exo js 1-2-3

--- ts/js01_variante/src/main.ts
+++ ts/js01_variante/src/main.ts
@@ -1,8 +1,28 @@
-const imageoff  = document.querySelector('img') as HTMLElement
+// création de la nouvelle image : 
+const imageOn = document.createElement('img') as HTMLImageElement;
+imageOn.id = "on";
+imageOn.src = "../assets/on.png";
+
+// création de la nouvelle image : 
+const imageOff = document.createElement('img') as HTMLImageElement;
+imageOff.id = "off";
+imageOff.src = "../assets/off.png";
+
+const  imgContainer= document.querySelector('.img-container') as HTMLBodyElement;
+imgContainer.appendChild(imageOff);
 
 const imageShow = (e : Event) => {
+    e.preventDefault();
+    const target = e.target as HTMLImageElement;
     
-}
+    if(target.id == "off" ){
+        imgContainer.removeChild(imageOff);
+        imgContainer.appendChild(imageOn);
+    } else if(target.id == "on"){
+        imgContainer.removeChild(imageOn);
+        imgContainer.appendChild(imageOff);
 
+}
+}
 document.addEventListener('click', imageShow)
 
