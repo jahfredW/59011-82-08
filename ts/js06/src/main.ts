bd562a24433d4fe5356c3335a9b96436198cc1b7
fred <fred.gruwe@gmail.com>
2023-09-05 17:14:45 +0200

    update

--- ts/js06/src/main.ts
+++ ts/js06/src/main.ts
@@ -1,155 +1,104 @@
-let currentX = 0;  // Pour garder la trace de la position actuelle en X
-let currentY = 0;  // Pour garder la trace de la position actuelle en Y
-
-
+let isDragging = false;
+let startX = 0, startY = 0;
+let initialX = 0, initialY = 0;
 
 const square = document.querySelector('[data-object="square"]') as HTMLElement;
 const squareContainer = document.querySelector('.container__square');
 const directionButtons = document.querySelectorAll('[data-button]');
 
-console.log(directionButtons)
-
-setTimeout(() => {
-    square?.classList.add('circle')
-}, 2000)
-
-// fonction d'animation du Blob ;) ;) 
+// Animate Square
 const animateSquare = () => {
-    if(square?.classList.contains('square')) {
-        square?.classList.remove('square');
-        square?.classList.add('circle');
+    if (square?.classList.contains('square')) {
+        square.classList.remove('square');
+        square.classList.add('circle');
     } else {
-        square?.classList.remove('circle');
-        square?.classList.add('square');
+        square.classList.remove('circle');
+        square.classList.add('square');
     }
 };
 
-
-let isDragging = false;
-                let startX = 0, startY = 0; // coordonnées du point de départ
-                let initialX = 0, initialY = 0; // positions initiales de la div
-// Cela supprime la nécessité d'utiliser setTimeout
-// car setInterval fera tout le travail
+// Animate Square every 1 second
 setInterval(animateSquare, 1000);
 
-const moveSquare = (e : Event, button : HTMLElement | null, square : HTMLElement) => {
-    if(square instanceof HTMLElement) {
+// Move Square Function
+const moveSquare = (e: Event, button: HTMLElement | null, square: HTMLElement | null) => {
+    if (square instanceof HTMLElement) {
         let posX = square.offsetLeft;
         let posY = square.offsetTop;
-        
-        if(button){
-            if(e instanceof MouseEvent) {
-                switch(button.getAttribute('data-button')) {
+
+        if (button) {
+            // Handle button clicks
+            if (e instanceof MouseEvent) {
+                switch (button.getAttribute('data-button')) {
                     case "right":
                         posX += 10;
-                        square.style.setProperty('--x-position', `${posX}px`);
                         break;
                     case "left":
                         posX -= 10;
-                        square.style.setProperty('--x-position', `${posX}px`);
                         break;
                     case "down":
                         posY += 10;
-                        square.style.setProperty('--y-position', `${posY}px`);
                         break;
                     case "up":
                         posY -= 10;
-                        square.style.setProperty('--y-position', `${posY}px`);
                         break;
-                    default:
-                        console.log('default');
                 }
             }
-        } else {
-            if(e instanceof KeyboardEvent) {
-                switch (e.key) {
-                    case "ArrowRight":
-                        posX += 10;
-                        square.style.setProperty('--x-position', `${posX}px`);
-                        break;
-                    case "ArrowLeft":
-                        posX -= 10;
-                        square.style.setProperty('--x-position', `${posX}px`);
-                        break;
-                    case "ArrowDown":
-                        posY += 10;
-                        square.style.setProperty('--y-position', `${posY}px`);
-                        break;
-                    case "ArrowUp":
-                        posY -= 10;
-                        square.style.setProperty('--y-position', `${posY}px`);
-                        break;
-                    default:
-                        console.log('default');
-                        return;
-                } 
-            } else if (e instanceof MouseEvent) {
-                
-                    if(square){
-                        if(e.type === 'mousedown'){
-                            isDragging = true;
-                            startX = e.clientX;
-                            startY = e.clientY;
-                            initialX = square.offsetLeft;
-                            initialY = square.offsetTop;
-                        }
-                        if(isDragging){
-                            if(e.type === 'mousemove'){
-                                const dx = e.clientX - startX;
-                                const dy = e.clientY - startY;
-                                square.style.left = (initialX + dx) + "px";
-                                square.style.top = (initialY + dy) + "px";
-                            }
-                        }
-                        if(e.type ==='mouseup'){
-                            isDragging = false;
-                        }
-    
-                    }
-
+        } else if (e instanceof KeyboardEvent) {
+            // Handle keyboard events
+            switch (e.key) {
+                case "ArrowRight":
+                    posX += 10;
+                    break;
+                case "ArrowLeft":
+                    posX -= 10;
+                    break;
+                case "ArrowDown":
+                    posY += 10;
+                    break;
+                case "ArrowUp":
+                    posY -= 10;
+                    break;
+            }
+        } else if (e instanceof MouseEvent) {
+            // Handle drag events
+            if (e.type === 'mousedown') {
+                isDragging = true;
+                startX = e.clientX;
+                startY = e.clientY;
+                initialX = posX;
+                initialY = posY;
+            }
+            if (e.type === 'mousemove' && isDragging) {
+                const dx = e.clientX - startX;
+                const dy = e.clientY - startY;
+                posX = initialX + dx;
+                posY = initialY + dy;
+            }
+            if (e.type === 'mouseup') {
+                isDragging = false;
             }
         }
-         
-        
 
-        // récupération de la largeur du conteneur 
-        let containerWidth = squareContainer?.getBoundingClientRect().width;
-        if(containerWidth){
-            console.log("containerWidth", containerWidth);
-            if( posX < 0 ){
-                posX = 0;
-            } else if (posX > containerWidth - 50 ){
-                posX = containerWidth - 50;
-            }
-            if(posY < 0 ){
-                posY = 0;
-            } else if (posY > 450 ){ {
-                posY = 450;
+        // Clamp positions within container
+        let containerWidth = squareContainer?.getBoundingClientRect().width || 0;
+        posX = Math.min(Math.max(posX, 0), containerWidth - 50);
+        posY = Math.min(Math.max(posY, 0), 450);
 
-            }
-        }
-        
-    
-        square.style.setProperty('--x-position', `${posX}px`);
-        square.style.setProperty('--y-position', `${posY}px`);
+        square.style.left = `${posX}px`;
+        square.style.top = `${posY}px`;
     }
-}
-}
+};
 
-
-directionButtons.forEach( button   => {
-    if(button instanceof HTMLElement) {
-        if(square){
-            button.addEventListener('click', (e) => moveSquare(e, button, square))
-        }
-        
-        
+// Button Events
+directionButtons.forEach(button => {
+    if (button instanceof HTMLElement) {
+        button.addEventListener('click', (e) => moveSquare(e, button, square));
     }
-})
+});
 
-
-document.addEventListener('keydown', (e) => moveSquare(e, null, square))
-document.addEventListener('mousedown', (e) => moveSquare(e, null, square))
-document.addEventListener('mousemove', (e) => moveSquare(e, null, square))
-document.addEventListener('mouseup', (e) => moveSquare(e, null, square))
-
+// Global Events
+document.addEventListener('keydown', (e) => moveSquare(e, null, square));
+document.addEventListener('mousedown', (e) => moveSquare(e, null, square));
+document.addEventListener('mousemove', (e) => moveSquare(e, null, square));
+document.addEventListener('mouseup', (e) => moveSquare(e, null, square));
