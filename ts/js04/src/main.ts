826373f9db7bdc314092dcf58678256eb107f827
fred <fred.gruwe@gmail.com>
2023-09-05 11:25:54 +0200

    Regular auto-commit at 11:25:52 on 05/09/2023

---
+++ ts/js04/src/main.ts
@@ -0,0 +1,25 @@
+const button  = document.querySelector('button') as HTMLButtonElement;
+const dessertListe = document.querySelector('ul') as HTMLElement;
+
+document.addEventListener('click', (e : Event) => {
+    let target = e.target as HTMLElement;
+    if (target.tagName === 'BUTTON') {
+        console.log(target);
+        let userInput = prompt('Entrez un dessert')
+        
+        // création d'une puce pour un dessert 
+        const dessertPuce = document.createElement('li') as HTMLElement;
+        dessertPuce.classList.add('dessert');
+        if(userInput){
+            dessertPuce.innerText = userInput;
+            dessertListe.appendChild(dessertPuce);
+        }
+        
+    } else if (target.tagName === 'LI') {
+        if(confirm('Voulez-vous vraiment supprimer cet dessert?')){
+            target.remove();
+        }
+    }
+
+    
+});
