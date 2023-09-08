2a282e46f9b4f5522b857d615b5ec7a616cbe45d
fred <fred.gruwe@gmail.com>
2023-09-08 09:57:12 +0200

    implémentation du pattern design Factory pour la génératin des bateaux enemis

--- ts/js06/src/main.ts
+++ ts/js06/src/main.ts
@@ -1,4 +1,4 @@
-import { SquareContainer, GamePad, Rect, Blob } from './classes/classes';
+import { SquareContainer, GamePad, ConcreteEnnemyShipFactory, Blob } from './classes/classes';
 
 // Instanciation des éléments 
 const squareContainer = new SquareContainer();
@@ -11,28 +11,50 @@ const blobElement = blob.getHtmlElement();
 const squareContainerElement = squareContainer.getHtmlElement();
 const directionButtons = gamePad.getHtmlElement();
 
+let lastTime = 0
+let accumulatedTime = 0;
+const shipSpawnRate = 2000;
+function gameLoop(timestamp : number) : void {
 
-let colorTab = ['red', 'green', 'blue', 'yellow', 'purple'];
-let x  = 100;
-let vInit = 1;
-let accel = 1;
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    accumulatedTime += deltaTime;
 
-let timer = 3000;
-function executeInterval() {
-    let i = Math.floor(Math.random() * 5);
-    const rect = new Rect();
-    rect.build(squareContainer);
-    rect.display(x, colorTab[i]);
-    squareContainer.addShape(rect);
-    vInit = Math.floor(Math.random() * 10) / 10 + 1;
-    accel = Math.floor(Math.random() * 10) / 100;
-    rect.move(vInit, accel);
-    x = Math.floor(Math.random() * 1000) + 1;
+    if (accumulatedTime >= shipSpawnRate) {
+        accumulatedTime -= shipSpawnRate;
+        const shipFactory = new ConcreteEnnemyShipFactory();
+        let cruiser = shipFactory.shipOrder("cruiser", squareContainer);
+    }
+  
+    // Mettez ici le code pour créer des bateaux, etc.
+    // Vous pouvez utiliser deltaTime pour ajuster le timing
+  
+    requestAnimationFrame(gameLoop);
+  }
+  
+requestAnimationFrame(gameLoop);
 
-    setTimeout(executeInterval, timer);
-}
 
-executeInterval();
+// let colorTab = ['cruiser'];
+// let x  = 100;
+// let vInit = 1;
+// let accel = 1;
+
+// let timer = 5000;
+// function executeInterval() {
+    
+//     // rect.build(squareContainer);
+//     // rect.display(x, colorTab[i]);
+//     // squareContainer.addShape(rect);
+//     // vInit = Math.floor(Math.random() * 10) / 10 + 1;
+//     // accel = Math.floor(Math.random() * 10) / 100;
+//     // rect.move(vInit, accel);
+//     // x = Math.floor(Math.random() * 1000) + 1;
+
+//     setTimeout(executeInterval, timer);
+// }
+
+// executeInterval();
 
 // Button Events
 directionButtons.forEach(button => {
