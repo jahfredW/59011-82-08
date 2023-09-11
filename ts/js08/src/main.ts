66b26f4ed71406f4f2687919f64d07b64e53c681
fred <fred.gruwe@gmail.com>
2023-09-11 16:27:29 +0200

    sql Ok

--- ts/js08/src/main.ts
+++ ts/js08/src/main.ts
@@ -3,34 +3,42 @@ const form  = document.querySelector<HTMLFormElement>('form');
 
 
 
-document.addEventListener('click', (e) => {
-    let target = e.target;
-    if(target instanceof Element) {
-        if(target.tagName === "BUTTON"){
-            if(form){
-                const formData = new FormData(form);
-                isValid(formData);
-            }
-        } 
-    }   
+document.addEventListener('input', (e) => {
+   if(form){
+    const formData = new FormData(form);
+    const target = e.target;
+    
+    if(target instanceof HTMLInputElement){
+        console.log(target.value)
+        console.log(target.getAttribute('data-input'))
+        if(target.getAttribute('data-input') === 'firstname'){
+            // gestion du firstName 
+            console.log(target.value)
+            alertCarBuilder(noSpecialCaracters(target.value));
+        }
+    }
+    
+   }
+   
 })
 
-// controller les saisies pour empêcher les erreurs de format 
-const isValid  = (formData : FormData ) : void => {
-   
-        for( const [key, value ] of formData){
-            console.log(key, value);
-            switch(key){
-                case "firstname":
-                    if(typeof(value) === "string"){
-                        alertCarBuilder(noSpecialCaracters(value));
-                    }
-                break;
-            }
 
-        }
+// // controller les saisies pour empêcher les erreurs de format 
+// const isValid  = (formData : FormData ) : void => {
+
+//         for( const [key, value ] of formData){
+//             console.log(key, value);
+//             switch(key){
+//                 case "firstname":
+//                     if(typeof(value) === "string"){
+//                         alertCarBuilder(noSpecialCaracters(value));
+//                     }
+//                 break;
+//             }
+
+//         }
       
-;}
+// ;}
 
 const noSpecialCaracters = (input : string) => {
     const regex = /^[a-zA-Z0-9]+$/;
