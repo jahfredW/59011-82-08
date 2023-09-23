d5898738e9cacf8864a0d0c4c64dc40fcb5788b4
fredator <fred.gruwe@gmail.com>
2023-09-23 12:26:42 +0200

    taquin 1.5 stable

--- js/taquin/taquin.js
+++ js/taquin/taquin.js
@@ -9,11 +9,12 @@ class Cell
     }
 
     // vérifie si une cellule peut bouger
-    move(grid) {
+    globalCheck(grid) {
+        this.checkWin(grid);
         // récupération des cellules à coté, et au dessus - dessous
         // recherche de la cellule vide : si dans juste à coté de la cellule courante, alors ok
         let currentCell = this;
-        let emptyCell = grid.cellTab.find(cell => cell.state == "empty");
+        let emptyCell = grid.cellTab.find(cell => cell.state == 0);
 
         // récupération 
 
@@ -23,44 +24,69 @@ class Cell
         console.log('tableau', grid.cellTab);
 
         // on compare les coordonnées : 
-        if ((emptyCell.x == currentCell.x && Math.abs(emptyCell.y - currentCell.y) == 1) 
-           || (emptyCell.y == currentCell.y && Math.abs(emptyCell.x - currentCell.x) == 1)){
-                console.log('ici');
-                this.switchPos(emptyCell);
-
-                //mise à jour du data-image
-
-                let emptyCellDiv = document.getElementById("cell-" + emptyCell.id);
-                emptyCellDiv.setAttribute('data-cell', currentCell.state);
-
-                let currentCellDiv = document.getElementById("cell-" + currentCell.id);
-                currentCellDiv.setAttribute('data-cell', "empty");
-
-                [currentCellDiv.innerHTML, emptyCellDiv.innerHTML] = [emptyCellDiv.innerHTML, currentCellDiv.innerHTML];
-
+        if ((emptyCell.x == this.x && Math.abs(emptyCell.y - this.y) == 1) 
+           || (emptyCell.y == this.y && Math.abs(emptyCell.x - this.x) == 1)){
+                this.switch(emptyCell);
         }
-
-        
-
-       
     }
 
-// // permutation des cellules : 
-switchPos(emptyCell) {
-    // let tempX = this.x;
-    // let tempY = this.y;
-    let tempState = this.state; // Egalement échanger les states
-    
-    // this.x = emptyCell.x;
-    // this.y = emptyCell.y;
-    this.state = emptyCell.state; // Egalement échanger les states
-    
-    // emptyCell.x = tempX;
-    // emptyCell.y = tempY;
-    emptyCell.state = tempState; // Egalement échanger les states
-}
+    //permutation des cellules : 
+    switch(emptyCell) {
+        
+        let tempState = this.state; // Egalement échanger les states
+        
+        // permuation des data-cells
+        let currentCellDiv = document.getElementById("cell-" + this.id);
+        currentCellDiv.setAttribute('data-cell', emptyCell.state);
 
+        let emptyCellDiv = document.getElementById("cell-" + emptyCell.id);
+        emptyCellDiv.setAttribute('data-cell', tempState);
+        
+        // permutation des states
+        this.state = emptyCell.state; // Egalement échanger les states
+        emptyCell.state = tempState; // Egalement échanger les states
 
+        // permtutation des innerHtml
+        [currentCellDiv.innerHTML, emptyCellDiv.innerHTML] = [emptyCellDiv.innerHTML, currentCellDiv.innerHTML];
+    }
+
+    displayWin(){
+        // on remove les listeners 
+        console.log("ici");
+        let cellsHtml = document.querySelectorAll(".cell");
+        if (cellsHtml){
+            cellsHtml.forEach( element => {
+                element.removeEventListener("click", () => this.globalCheck(grid));
+            });
+        }
+    }
+
+    /**
+     * vérifie si le joueur a gagné
+     * @param {array} grid 
+     */
+    checkWin(grid){
+       let isSorted = this.isSorted(grid.cellTab, "state");
+       
+       if(isSorted){
+          this.displayWin()
+       }
+    }
+    /**
+     * Permet de savoir si un taleau est trié 
+     * @param { array } array 
+     * @param { property} property 
+     * @returns { boolean } // indique si le tableau est trié ou nom. 
+     */
+    isSorted(array, property){
+        for( let index = 0; index < array.length - 1; index ++ ){
+            console.log(array[index + 1][property])
+            if(array[index + 1][property] < array[index][property]){
+                return false;
+            }
+        }
+        return true;
+    }
 
     // représentation html de la cellule
     cellHTMLDisplay(grid){
@@ -73,12 +99,11 @@ switchPos(emptyCell) {
 
         // ajout d'un eventListener 
 
-        cellHtml.addEventListener("click", () => this.move(grid))
-        if( this.state != "empty"){
+        cellHtml.addEventListener("click", () => this.globalCheck(grid))
+        if( this.state != 0){
              cellHtml.innerHTML = this.state;
         }
        
-
         return cellHtml;
     }
 }
@@ -115,14 +140,14 @@ class Grid
         const ids = this.cellTab.map( cell => cell.id);
         console.log(ids);
         // tri des ids 
-        ids.sort(() => Math.random() - 0.5);
+        // ids.sort(() => Math.random() - 0.5);
         console.log(ids);
         // ré attribution des ids 
         this.cellTab.forEach( (cell, index) => {
             cell.state = ids[index];
         }  )
-        let randomStateNumber = Math.floor(Math.random() * (this.cellTab.length + 1));
-        this.cellTab[randomStateNumber].state = "empty";
+        // let randomStateNumber = Math.floor(Math.random() * (this.cellTab.length + 1));
+        // this.cellTab[randomStateNumber].state = "empty";
 
     }
 
@@ -139,9 +164,6 @@ class Grid
 }
 
 let grid = new Grid();
-
-console.log(grid.cellTab);
-
 grid.gridHTMLDisplay();
 
 
