1bdb567075077d864abc09f953438633a72131a0
fred <fred.gruwe@gmail.com>
2023-09-25 11:25:56 +0200

    Regular auto-commit at 11:25:55 on 25/09/2023

--- ts/js04/src/main.ts
+++ ts/js04/src/main.ts
@@ -3,6 +3,8 @@ const dessertListe = document.querySelector('ul') as HTMLElement;
 
 document.addEventListener('click', (e : Event) => {
     let target = e.target as HTMLElement;
+
+    // gestion du bouton 
     if (target.tagName === 'BUTTON') {
         console.log(target);
         let userInput = prompt('Entrez un dessert')
@@ -14,7 +16,8 @@ document.addEventListener('click', (e : Event) => {
             dessertPuce.innerText = userInput;
             dessertListe.appendChild(dessertPuce);
         }
-        
+    
+        // gestion de la suppression de liste 
     } else if (target.tagName === 'LI') {
         if(confirm('Voulez-vous vraiment supprimer cet dessert?')){
             target.remove();
